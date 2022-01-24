7c1c53ccce7014b6f2b46ed493df31b32155ae04
Michał Frąckiewicz <hello@systemz.pl>
2022-01-25 00:24:27 +0100

    Live updates with Turbo for Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :location
   belongs_to :brand
+
+  after_create_commit { broadcast_append_to 'item' }
+  after_update_commit { broadcast_replace_to 'item' }
+  after_destroy_commit { broadcast_remove_to 'item' }
 end
