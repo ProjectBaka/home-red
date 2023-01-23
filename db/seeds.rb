390bf8d88a1e99ea889f2479f24721867c769911
Bartosz Stefańczyk <bartosz@cdfn.dev>
2023-01-23 12:41:05 +0100

    Add basic seeds (#4)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+locations = Location.create([{ name: "Fridge" }, { name: "Cupboard" }, { name: "Basement" }])
+brands = Brand.create([{ name: "Coca-Cola" }, { name: "Nestle" }, { name: "Homemade" }])
+Item.create(
+  [
+    { name: "Coca-Cola Zero", brand: brands.first, barcode: "5449000131836", valid_to: 1.years.from_now, location: locations.first },
+    { name: "Oat Flakes", brand: brands.second, barcode: "4606272022663", valid_to: 3.years.from_now, location: locations.second },
+    { name: "Pickles", brand: brands.third, valid_to: 15.years.from_now, location: locations.third },
+  ]
+)
