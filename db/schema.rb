ed28c7da878cdef57cbdb12cbdf26f088776b62c
Bartosz Stefańczyk <bartosz@cdfn.dev>
2023-01-23 11:44:46 +0100

    Upgrade to ruby 3.2.0 and bump dependency versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_154324) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_24_154324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "timescaledb"
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.text "notes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "notes"
     t.string "barcode"
-    t.datetime "valid_to", precision: 6
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "valid_to"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "location_id"
     t.integer "brand_id"
   end
@@ -37,8 +35,8 @@ ActiveRecord::Schema.define(version: 2022_01_24_154324) do
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.text "notes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
